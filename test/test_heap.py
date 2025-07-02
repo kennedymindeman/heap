d3c6fc213f80d532eb19b2d3f44429031a270f12
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-01 21:29:32 -0500

    Add test for heap size

--- test/test_heap.py
+++ test/test_heap.py
@@ -20,3 +20,8 @@ def test_heap_is_not_empty_after_inserting() -> None:
     heap = Heap()
     heap.insert(0)
     assert not heap.is_empty()
+
+
+def test_heap_size_is_0_on_initialization() -> None:
+    heap = Heap()
+    assert heap.size() == 0
