c7d72ccf262c7354f037b6d89d99608f3f880008
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-01 21:21:22 -0500

    Add test for when heap is not empty

--- test/test_heap.py
+++ test/test_heap.py
@@ -14,3 +14,9 @@ def test_add_to_empty_heap() -> None:
 def test_heap_is_empty_on_initialization() -> None:
     heap = Heap()
     assert heap.is_empty()
+
+
+def test_heap_is_not_empty_after_adding() -> None:
+    heap = Heap()
+    heap.add(0)
+    assert not heap.is_empty()
