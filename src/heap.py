9c2989ee4f2c733668cf62c3cc39812fa6ab5c7d
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-01 20:37:51 -0500

    Add tests for heap add method and stub out heap add method

--- src/heap.py
+++ src/heap.py
@@ -1,3 +1,6 @@
 class Heap:
     def __init__(self) -> None:
         self.root = None
+
+    def add(self, item) -> None:
+        raise NotImplemented
--- test/test_heap.py
+++ test/test_heap.py
@@ -3,3 +3,8 @@ from src.heap import Heap
 
 def test_initialization() -> None:
     heap = Heap()
+
+
+def test_add() -> None:
+    heap = Heap()
+    heap.add(0)
