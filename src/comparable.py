67946edf329435d2afcb12d49b758bb17f80c3c1
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-08 23:26:07 -0500

    Change method definitions in Comparable to use Any

--- src/comparable.py
+++ src/comparable.py
@@ -1,16 +1,15 @@
-from __future__ import annotations
-from typing import Protocol
+from typing import Any, Protocol
 
 
 class Comparable(Protocol):
-    def __lt__(self, other: Comparable) -> bool:
+    def __lt__(self, other: Any) -> bool:
         ...
 
-    def __gt__(self, other: Comparable) -> bool:
+    def __gt__(self, other: Any) -> bool:
         ...
 
-    def __le__(self, other: Comparable) -> bool:
+    def __le__(self, other: Any) -> bool:
         ...
 
-    def __ge__(self, other: Comparable) -> bool:
+    def __ge__(self, other: Any) -> bool:
         ...
